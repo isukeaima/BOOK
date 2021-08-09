db87d2bfe8959e4790ae1b32eb736faf20101386
isukeaima <asaumi.kei49@gmail.com>
2021-08-09 07:31:07 +0000

    finish

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,20 +1,22 @@
 class BooksController < ApplicationController
   def index
-    @book = Book.all
+    @books = Book.all
+    @book = Book.new
   end
 
   def show
     @book = Book.find(params[:id])
   end
 
-  def new
-   @book = Book.new
-  end
-
   def create
     @book = Book.new(book_params)
-    @book.save
+    @books = Book.all
+   if @book.save
+     flash[:notice] ="Book was successfully created"
     redirect_to book_path(@book.id)
+    else
+    render action: :index
+  end
   end
 
   def edit
@@ -22,15 +24,23 @@ class BooksController < ApplicationController
   end
   
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] ="Book was successfully updated"
+    redirect_to book_path(@book)
+    else
+    render action: :edit
+  end
   end
   
   def destroy
     book = Book.find(params[:id])
-    book.destroy
+    if book.destroy
+      flash[:notice] ="Book was successfully delete"
     redirect_to books_path
+    else
+    render action: :index
+  end
   end
 
   private
